a0fc0810e45cdf2171c17bbdbb2d95baa7af0c62
d1larafaizullina <d1larafaizullina@yandex.ru>
2022-05-06 13:52:45 +0500

    docflow Initial commit. fw.

--- flask_app.py
+++ flask_app.py
@@ -8,6 +8,7 @@ app.config['SECRET_KEY'] = 'secret key'
 
 def main():
     db_session.global_init("db/data.sqlite")
+    db_file = ("db/docs.db")
     app.run(port=8080, host='127.0.0.1')  # , debug=True)
 
 
